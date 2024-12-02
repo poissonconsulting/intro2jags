e8744845373faaf0927ee9aba254cb5555183ad9
Nicole Hill <nicole@poissonconsulting.ca>
2024-12-02 10:04:27 -0800

    Package skeleton

---
+++ R/params.R
@@ -0,0 +1,16 @@
+#' Parameter Descriptions
+#'
+#' Default parameter descriptions which may be overridden in individual
+#' functions.
+#'
+#' A flag is a non-missing logical scalar.
+#'
+#' A string is a non-missing character scalar.
+#
+#' @inheritParams rlang::args_dots_empty
+#' @keywords internal
+#' @aliases parameters arguments args
+#' @usage NULL
+# nocov start
+params <- function(...) NULL
+# nocov end
